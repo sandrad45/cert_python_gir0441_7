ef950c18fed89a2f79b50dd450cebd4f4c06867c
sandrad45 <114195767+sandrad45@users.noreply.github.com>
2022-10-06 10:36:19 -0500

    examen de mod 3

---
+++ Modulo3/Lab_3_4_1_13.py
@@ -0,0 +1,32 @@
+'''
+Descripcion:Operadores de comparación y ejecución condicional
+Autor: Gonzalez Mazano Sandra Dania 
+Fecha: 27 sep 2022
+'''
+# paso 1
+Beatles=[]
+print("Paso 1:", Beatles)
+
+# paso 2
+Beatles.Oppend ("John")
+Beatles.Oppend ("Paul")
+Beatles.Oppend ("Geoge")
+print("Paso 2:", Beatles)
+
+# paso 3
+for miembros in rango (2):
+    Beatles.append (input("Nuevos miemvros de la banda:")
+print("Paso 3:", Beatles)
+
+# paso 4
+del Beatles[-1]
+del Beatles[-1]
+print("Paso 4:", Beatles)
+
+# paso 5
+Beatles.insert (0,"Ringo Starr")
+print("Paso 5:", Beatles)
+
+
+# probando la longitud de la lista
+print("Los Fav", len(Beatles))
